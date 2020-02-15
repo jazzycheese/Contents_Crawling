75d4db65d0b7a1be155881e7d2c4da21842b56d9
Soyeon Jung <soyeon.lily@gmail.com>
2020-02-15 16:33:02 +0900

    edit save

--- han_category.py
+++ han_category.py
@@ -10,7 +10,7 @@ write_wb = Workbook()
 write_ws = write_wb.active
 
 print('\n-----지정한 셀 출력-----')
-get_cells = load_ws['A1':'FY1']
+get_cells = load_ws['A1':'FY1']   
 for row in get_cells:
     for cell in row:
         #print(cell.value)       #print (type(cell.value)) <class 'str'>
@@ -22,8 +22,4 @@ for row in get_cells:
         for tag in soup.select('p[class=category]'):
             print(tag.text) 
             write_ws.append([tag.text])
-            write_wb.save('Result.xlsx')
-
-
-#for tag in soup.select("p", align_="justify"):
-#        print(tag.text)
+write_wb.save('Result.xlsx')
